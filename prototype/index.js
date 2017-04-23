1022b35660dc4f08311fbc2723702c8421b2c92a
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2017-04-23 02:49:51 -0400

    got the open to work, with problems - positioning is off - there is a race condition between close and open

--- prototype/index.js
+++ prototype/index.js
@@ -1,13 +1,33 @@
 function close() {
-  // $(".preview-pane").hide(10);
-  $("#preview-pane-container").hide(100, function() {
-  // $("#preview-pane-container").slideUp(200, function() {
-    $(".image-card-selected").animate({ marginBottom: "10px" }, 200);
-  });
+  console.log("closing");
+  if ($(".image-card-selected").length) {
+    $("#preview-pane-container").hide(100, function () {
+      $(".image-card-selected").animate({ marginBottom: "10px" }, 200);
+      $(".image-card-selected").removeClass("image-card-selected");
+    });
+  }
+}
+
+function open(target) {
+  close();
+
+  console.log("opening");
+
+  target.animate({ marginBottom: "660px" }, 200);
+  target.addClass("image-card-selected");
+  imageOffset = target.offset();
+  $("#preview-pane-container").css(
+    "top", imageOffset.top + target.height + 20
+    // "left", imageOffset.left + (target.width / 2)
+  );
+  $("#preview-pane-container").show(100);
 }
 
 $(document).ready(function () {
 
-  $(".preview-pane-close-button ").on("click", close); 
+  $(".preview-pane-close-button ").on("click", close);
+  $(".image-card").on("click", function () {
+    open($(this));
+  });
 
 });
