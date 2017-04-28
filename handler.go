0607824cf9dc69f266a9af6bfc95f724ccf8cf66
Jack Hayhurst <jhayhurst@liquidweb.com>
2017-04-28 18:10:06 -0400

    added a directory handler that's based on templates

--- handler.go
+++ handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io"
 	"net/http"
 	"os"
@@ -10,37 +11,91 @@ import (
 
 // ServeFile serves a given file back to the requester, and determines content type by algorithm only.
 // It does not use the file's extension to determine the content type.
-func ServeFile(w http.ResponseWriter, r *http.Request) {
-	f, err := os.Open(filepath.Clean(r.URL.Path))
-	if err != nil {
-		http.Error(w, fmt.Sprintf("%s not found", r.URL.Path), http.StatusNotFound)
+func ServeFile(basePath string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open(filepath.Join(basePath, r.URL.Path))
+		if err != nil {
+			http.Error(w, fmt.Sprintf("not found: %s", r.URL.Path), http.StatusNotFound)
+			return
+		}
+
+		stat, err := f.Stat()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("cannot read file: %s", r.URL.Path), http.StatusForbidden)
+			return
+		}
+
+		chunk := make([]byte, 512)
+
+		_, err = f.Read(chunk)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("cannot read file: %s", r.URL.Path), http.StatusForbidden)
+			return
+		}
+
+		_, err = f.Seek(0, io.SeekStart)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("cannot read file: %s", r.URL.Path), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", http.DetectContentType(chunk))
+		http.ServeContent(w, r, r.URL.Path, stat.ModTime(), f)
+
 		return
+
 	}
+}
 
-	chunk := make([]byte, 512)
+// ServeFolder lists all files in a directory, and passes them to template execution to build a directory listing.
+func ServeFolder(basePath string, templ *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open(filepath.Join(basePath, r.URL.Path))
+		if err != nil {
+			http.Error(w, fmt.Sprintf("not found: %s", r.URL.Path), http.StatusNotFound)
+			return
+		}
 
-	_, err = f.Read(chunk)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("cannot read file %s", r.URL.Path), http.StatusForbidden)
-		return
+		stat, err := f.Stat()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("cannot read target: %s", r.URL.Path), http.StatusInternalServerError)
+			return
+		}
+
+		if !stat.IsDir() {
+			ServeFile(basePath)(w, r)
+			return
+		}
+
+		contents, err := f.Readdir(0)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("cannot read directory: %s", r.URL.Path), http.StatusForbidden)
+			return
+		}
+
+		var data struct {
+			files []string
+			dirs  []string
+			all   []string
+		}
+
+		for _, each := range contents {
+			data.all = append(data.all, each.Name())
+			switch each.IsDir() {
+			case true:
+				data.dirs = append(data.dirs, each.Name())
+			default:
+				data.files = append(data.files, each.Name())
+			}
+		}
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		err = templ.Execute(w, data)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("error building response: %s", r.URL.Path), http.StatusInternalServerError)
+			return
+		}
 	}
-
-	_, err = f.Seek(0, io.SeekStart)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("cannot read file %s", r.URL.Path), http.StatusInternalServerError)
-		return
-	}
-
-	stat, err := f.Stat()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("cannot read file %s", r.URL.Path), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", http.DetectContentType(chunk))
-	http.ServeContent(w, r, r.URL.Path, stat.ModTime(), f)
-
-	return
 }
 
 func main() {
