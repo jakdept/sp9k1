935821f92457e44fa557f3c1300aaf9b50845d0a
Jack Hayhurst <jakdept@gmail.com>
2017-12-03 01:25:31 -0500

    made some changes, correcting a lot of things
    - the updated version isn't handling thumbs and directories other than literal directories properly

--- public/page.js
+++ public/page.js
@@ -1,21 +1,21 @@
 function close() {
   // $(".preview-container").hide();
-  document.getElementById("preview-container").style.display = "none";
+  $("#preview-container").style.display = "none";
   $(".image-container").removeClass("blur");
 }
 
 function open(target) {
   console.log("opening an image");
-  document.getElementById("preview-pane").src = target.getAttribute("data-original");
-  document.getElementById("preview-caption").innerHTML = target.alt;
-  document.getElementById("preview-container").style.display = "flex";
+  $("#preview-pane").src = target.getAttribute("data-original");
+  $("#preview-caption").innerHTML = target.alt;
+  $("#preview-container").style.display = "flex";
   $(".image-container").addClass("blur");
   // $("#preview-container").show(0);
 }
 
 $(document).ready(function () {
 
-  document.getElementById("preview-container").onclick = close;
+  $("#preview-container").onclick = close;
   // imageCards = document.getElementsByClassName("image-card");
   // for (var i = 0; i < imageCards.length; i++) {
   //   imageCards[i].onflick = function() {
@@ -39,4 +39,44 @@ $(document).ready(function () {
       close();
     }
   });
+});
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+var filterImages = debounce(function () {
+  close();
+  var filterString = $("#filter").value;
+  // show all images
+  $("img").each(function( index ) {
+    $( this ).show(0);
+  });
+
+  // filter to images that match and hide them
+  $("img").filter(function( index ) {
+    return $( this ).dataset.original.indexOf(filterString) > -1
+  }).each(function( index ) {
+    $( this ).hide(0);
+  });
+}, 250);
+
+$("#filter").on("input", function () {
+  filterImages();
 });
