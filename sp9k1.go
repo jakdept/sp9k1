00e1bad7260e142c41feb537e5fe53d62ed80a1b
Jack Hayhurst <jakdept@gmail.com>
2018-04-14 22:01:34 -0400

    rewrote server to work with letsencrypt changes
    Signed-off-by: Jack Hayhurst <jakdept@gmail.com>

--- sp9k1.go
+++ sp9k1.go
@@ -3,6 +3,8 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
 	"fmt"
 	"html/template"
 	"io/ioutil"
@@ -23,10 +25,13 @@ import (
 // now would this be shitposting if there were _tests_?
 
 var (
-	listenAddress  = kingpin.Flag("listen", "non-TLS listen addresses").Short('l').Default("127.0.0.1:8080").TCPList()
-	enableTLS      = kingpin.Flag("tls", "enables auto-TLS and push to https").Default("false").Bool()
-	domain         = kingpin.Flag("domain", "domain to register, startup, and redirect to").String()
-	imgDir         = kingpin.Flag("images", "directory of images to serve").Short('i').Default("./").ExistingDir()
+	// common options
+	enableTLS = kingpin.Flag("tls", "enables auto-TLS and push to https").Default("false").Bool()
+	port      = kingpin.Flag("port", "port to listen on").Default("80").Int()
+	domain    = kingpin.Flag("domain", "domain/ip to listen on").Default("localhost").String()
+	imgDir    = kingpin.Flag("images", "directory of images to serve").Short('i').Default("./").ExistingDir()
+
+	// less common options
 	cacheMinDays   = kingpin.Flag("cacheMin", "minimum days to cache images in browser").Default("30").Int()
 	cacheVariation = kingpin.Flag("cacheDayVariation", "browser cache variation").Default("7").Int()
 	thumbWidth     = kingpin.Flag("width", "maximum thumbnail width").Default("310").Int()
@@ -76,7 +81,7 @@ func buildMuxer(logger *log.Logger,
 	// building the static handler
 	h = http.FileServer(fs)
 	// split the main folder off into a redirect
-	h = dandler.Split(http.RedirectHandler("/", 302), h)
+	h = dandler.Split(http.RedirectHandler("/", 301), h)
 	// add a prefix before the handler
 	h = http.StripPrefix("/static/", h)
 	// add some expiration
@@ -88,7 +93,7 @@ func buildMuxer(logger *log.Logger,
 	// create a caching handler
 	h = dandler.ThumbCache(logger, *thumbWidth, *thumbHeight, 32<<20, *imgDir, "thumbs", "jpg")
 	// split the folder itself into a redirect
-	h = dandler.Split(http.RedirectHandler("/", 302), h)
+	h = dandler.Split(http.RedirectHandler("/", 301), h)
 	// strip the prefix
 	h = http.StripPrefix("/thumb/", h)
 	// add some expiration
@@ -105,25 +110,73 @@ func buildMuxer(logger *log.Logger,
 
 	h = mux
 
-	// add canonical header if required
-	if *domain != "" {
-		options := 0
-		options += dandler.ForceHost
-		if *enableTLS {
-			options += dandler.ForceHTTPS
-			h = dandler.CanonicalHost(fmt.Sprintf("%s:443", *domain), options, h)
-		} else {
-			h = dandler.CanonicalHost(fmt.Sprintf("%s:%d", *domain,
-				(*listenAddress)[0].IP), options, h)
-		}
-	}
-
 	h = handlers.CombinedLoggingHandler(os.Stdout, h)
 
 	// compress responses
 	h = handlers.CompressHandler(h)
 
 	return h
+}
+
+func launchServers(h http.Handler, d chan struct{}, e chan<- error) {
+	if !*enableTLS {
+		// start other http listeners
+		s := &http.Server{
+			Addr:    fmt.Sprintf(":%d", *port),
+			Handler: h,
+		}
+		if *domain == "localhost" {
+			s.Addr = fmt.Sprintf("%s:%d", *domain, *port)
+		}
+		go func() {
+			defer close(d)
+			e <- s.ListenAndServe()
+		}()
+
+		go func() {
+			<-d
+			s.Shutdown(context.Background())
+		}()
+		return
+	}
+
+	cert := &autocert.Manager{
+		Cache:      autocert.DirCache("secret-dir"),
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(*domain),
+	}
+
+	canonicalURL := fmt.Sprint("https://", *domain, ":", *port)
+	// start a port 80 listener for cert
+	// create the http server on port 80
+	srvHTTP := &http.Server{
+		Addr:    ":http",
+		Handler: cert.HTTPHandler(http.RedirectHandler(canonicalURL, 301)),
+	}
+	go func() {
+		defer close(d)
+		e <- srvHTTP.ListenAndServe()
+	}()
+
+	// create the https server
+	srvHTTPS := &http.Server{
+		Addr:      fmt.Sprintf(":%d", *port),
+		Handler:   h,
+		TLSConfig: &tls.Config{GetCertificate: cert.GetCertificate},
+	}
+	if *domain == "localhost" {
+		srvHTTPS.Addr = fmt.Sprintf("%s:%d", *domain, *port)
+	}
+	go func() {
+		defer close(d)
+		e <- srvHTTPS.ListenAndServeTLS("", "")
+	}()
+
+	go func() {
+		<-d
+		srvHTTP.Shutdown(context.Background())
+		srvHTTPS.Shutdown(context.Background())
+	}()
 }
 
 func main() {
@@ -138,28 +191,24 @@ func main() {
 	}
 
 	logger := log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Llongfile)
+	var errChan chan error
+	go func() {
+		for e := range errChan {
+			logger.Fatal(e)
+		}
+	}()
+
+	done := make(chan struct{})
+	defer close(done)
 
 	fs := createStaticFS(logger, *staticDir)
 
 	templ := parseTemplate(logger, fs)
 
-	done := make(chan struct{})
-	defer close(done)
-
 	srvHandlers := buildMuxer(logger, fs, templ, done)
 
-	var errChan chan error
-	go func() {
-		for _, address := range *listenAddress {
-			errChan <- http.ListenAndServe(address.String(), srvHandlers)
-		}
-	}()
-	if *enableTLS {
-		go func() {
-			errChan <- http.Serve(autocert.NewListener(*domain), srvHandlers)
-		}()
-	}
-	for e := range errChan {
-		log.Fatal(e)
-	}
+	fmt.Println("launching servers")
+	launchServers(srvHandlers, done, errChan)
+	fmt.Println("launched servers")
+	<-done
 }
